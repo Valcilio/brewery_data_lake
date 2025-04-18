b93958596b3c4cc4e42ca22ef10d297ea9ebe296
valcilio <eugeniovalcilio@gmail.com>
2025-04-18 14:30:28 -0300

    feat: adding region to cloudwatch handler

--- domain/utils/etllogger.py
+++ domain/utils/etllogger.py
@@ -25,7 +25,7 @@ class ETLLogger:
 
         handlers = [
             logging.StreamHandler(),
-            cloudwatch.CloudwatchHandler(log_group="/aws/ec2/brewery_etl_ec2"),
+            cloudwatch.CloudwatchHandler(log_group="/aws/ec2/brewery_etl_ec2", region="us-east-1"),
         ]
 
         logging.basicConfig(
